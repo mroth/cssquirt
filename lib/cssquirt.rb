66c560544203517d069e4739aaab6f39bf9b90b5
Matthew Rothenberg <mrothenberg@gmail.com>
2013-06-04 17:58:39 -0400

    basic class

--- lib/cssquirt.rb
+++ lib/cssquirt.rb
@@ -1,5 +1,15 @@
 require "cssquirt/version"
+require "base64"
 
-  # Your code goes here...
 module CSSquirt
+  class CSSquirter
+    # def initialize
+    # end
+    
+    # Public: Encodes a file into Base64 string representation
+    def self.encode_file(file_path)
+      Base64.strict_encode64(File.read file_path)
+    end
+
+  end
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require 'rspec'
+require 'cssquirt'
+
+RSpec.configure do |config|
+  config.color_enabled = true
+  config.formatter     = 'documentation'
+end
