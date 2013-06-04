07c29901712841cd2da8cf243f91c56c8325b796
Matthew Rothenberg <mrothenberg@gmail.com>
2013-06-04 19:43:26 -0400

    move image class into own file

--- lib/cssquirt.rb
+++ lib/cssquirt.rb
@@ -1,4 +1,5 @@
 require "cssquirt/version"
+require "cssquirt/image_file"
 
 module CSSquirt
   class CSSquirter
@@ -25,29 +26,5 @@ module CSSquirt
       ImageFile.new(file_path).raw_encode
     end
 
-  end
-
-  class ImageFile
-    require "base64"
-
-    def initialize(file_path)
-      @file_path = file_path
-    end
-
-    def as_img_tag()
-      "<img src='#{self.encode}' />"
-    end
-
-    def as_css_background()
-      "background: url(#{self.encode}) no-repeat;"
-    end
-
-    def encode()
-      "data:image/png;base64," + self.raw_encode
-    end
-
-    def raw_encode()
-      Base64.strict_encode64(File.read @file_path)
-    end
   end
 end
---
+++ lib/cssquirt/image_file.rb
@@ -0,0 +1,25 @@
+module CSSquirt
+  class ImageFile
+    require "base64"
+
+    def initialize(file_path)
+      @file_path = file_path
+    end
+
+    def as_img_tag()
+      "<img src='#{self.encode}' />"
+    end
+
+    def as_css_background()
+      "background: url(#{self.encode}) no-repeat;"
+    end
+
+    def encode()
+      "data:image/png;base64," + self.raw_encode
+    end
+
+    def raw_encode()
+      Base64.strict_encode64(File.read @file_path)
+    end
+  end
+end
