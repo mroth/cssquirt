d888a1cb8fdddce8bd26d988fed2e7433865b5f8
Matthew Rothenberg <mrothenberg@gmail.com>
2013-06-04 18:08:46 -0400

    encode in expected css format

--- lib/cssquirt.rb
+++ lib/cssquirt.rb
@@ -5,9 +5,16 @@ module CSSquirt
   class CSSquirter
     # def initialize
     # end
-    
-    # Public: Encodes a file into Base64 string representation
+
+    # Public: Encodes a file into a CSS string representation of it
     def self.encode_file(file_path)
+      prefix = "data:image/png;base64,"
+      suffix = ""
+      prefix + self.raw_encode_file(file_path) + suffix
+    end
+
+    # Public: Encodes a file into its raw Base64 string representation
+    def self.raw_encode_file(file_path)
       Base64.strict_encode64(File.read file_path)
     end
 
