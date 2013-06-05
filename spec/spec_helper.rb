cd14c714a3bde0676887ca07ddfd7f3378e42181
Matthew Rothenberg <mrothenberg@gmail.com>
2013-06-05 01:02:16 -0400

    suppress stderr/stdout during rspec tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,4 +5,24 @@ include CSSquirt
 RSpec.configure do |config|
   config.color_enabled = true
   config.formatter     = 'documentation'
+
+  config.before(:all) { silence_output }
+  config.after(:all)  { enable_output }
+end
+
+
+# Redirects stderr and stdout to /dev/null.
+# Helpers from https://gist.github.com/adamstegman/926858
+def silence_output
+  @orig_stderr = $stderr
+  @orig_stdout = $stdout
+  $stderr = File.new('/dev/null', 'w')
+  $stdout = File.new('/dev/null', 'w')
+end
+
+def enable_output
+  $stderr = @orig_stderr
+  $stdout = @orig_stdout
+  @orig_stderr = nil
+  @orig_stdout = nil
 end
